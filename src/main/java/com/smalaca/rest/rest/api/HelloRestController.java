522990ed2104caca0584482aef5aaaedc32065c7
smalaca <ab13.krakow@gmail.com>
2022-01-15 09:41:01 +0100

    introduce name path variable to HelloRestController method

--- src/main/java/com/smalaca/rest/rest/api/HelloRestController.java
+++ src/main/java/com/smalaca/rest/rest/api/HelloRestController.java
@@ -1,14 +1,15 @@
 package com.smalaca.rest.rest.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/invitations")
 public class HelloRestController {
-    @GetMapping
-    public String sayHi() {
-        return "Hi!";
+    @GetMapping("/{name}")
+    public String sayHi(@PathVariable String name) {
+        return "Hi " + name + "!";
     }
 }
